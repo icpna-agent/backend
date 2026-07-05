d9cebf62dfe5e096465e0ab6449fd2aed8b9779e
WettiestMan <S_4r8ol@protonmail.com>
2026-07-05 02:17:11 -0500

    feat: add health endpoint for backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,37 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CurrentUser } from './core/decorators/user.decorator';
 import type { AccessTokenPayload } from './modules/auth/dto/auth.return.types';
+import { Public } from './core/decorators/public.decorator';
+import { ApiReturn } from './core/types/core.types';
+
+export type AppHealthStatusLevels = 'up' | 'weak' | 'down';
+
+export interface AppHealthStatusInfo {
+  status: AppHealthStatusLevels;
+  latency?: number;
+  error?: string;
+}
+
+export interface AppHealthStatus {
+  status: AppHealthStatusLevels;
+  timestamp: string;
+  checks: {
+    database: AppHealthStatusInfo;
+  }
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("hello")
+  @Get('hello')
   getHello(@CurrentUser() user: AccessTokenPayload): string {
     return this.appService.getHello(user);
+  }
+
+  @Public()
+  @Get('health')
+  async check(): Promise<ApiReturn<AppHealthStatus>> {
+    return this.appService.check();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { AccessTokenPayload } from './modules/auth/dto/auth.return.types';
+import { ApiReturn } from './core/types/core.types';
+import { AppHealthStatus, AppHealthStatusInfo } from './app.controller';
+import { performance } from 'node:perf_hooks';
+import { database } from './db/connection';
 
 @Injectable()
 export class AppService {
   getHello(user: AccessTokenPayload): string {
     return `Hello World! ${user.user}, ${user.id}, ${user.phone}`;
+  }
+
+  async check(): Promise<ApiReturn<AppHealthStatus>> {
+    const checks: Record<string,AppHealthStatusInfo> = {};
+
+    let isHealthy = true;
+
+    try {
+      const start = performance.now();
+      await database.execute('SELECT 1;');
+      checks['database'] = {
+        status: 'up',
+        latency: performance.now() - start,
+      };
+    } catch (error: unknown) {
+      checks['database'] = {
+        status: 'down',
+        error: 'Connection failed',
+      };
+      isHealthy = false;
+    }
+
+    return {
+      error: !isHealthy,
+      body: {
+        status: isHealthy ? 'up' : 'down',
+        timestamp: new Date().toISOString(),
+        checks: {
+          database: checks['database'],
+        },
+      },
+    };
   }
 }
