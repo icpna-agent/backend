9647f62d60a7c2167bbc034424bda1764221c49e
Erick Flores Santos <xerickcua@gmail.com>
2026-07-10 18:51:39 -0500

    .

--- src/db/config.ts
+++ src/db/config.ts
@@ -8,5 +8,5 @@ export const dbConfig = {
     user: process.env.DB_USER ?? 'user',
     password: process.env.DB_PASSWORD ?? '',
     database: process.env.DB_NAME ?? 'database',
-    ssl: false, // Disable SSL for local development
+    ssl: process.env.PROD === 'true',
 };
