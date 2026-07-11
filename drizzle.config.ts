45dca85e8a8c5c710d3bd8a5affa89bb0f2d5f63
WettiestMan <S_4r8ol@protonmail.com>
2026-07-10 19:11:43 -0500

    chore: testing stuff 3

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,5 +5,14 @@ export default {
     schema: "./src/db/tables/*.table.ts",
     out: "./drizzle",
     dialect: "postgresql",
-    dbCredentials: dbConfig,
+    dbCredentials: {
+        host: process.env.DB_HOST ?? 'localhost',
+        port: parseInt(process.env.DB_PORT ?? '5432'),
+        user: process.env.DB_USER ?? 'user',
+        password: process.env.DB_PASSWORD ?? '',
+        database: process.env.DB_NAME ?? 'database',
+        ssl: process.env.NODE_ENV === 'production' ? {
+            rejectUnauthorized: false,
+        } : false,
+    },
 } satisfies Config;
